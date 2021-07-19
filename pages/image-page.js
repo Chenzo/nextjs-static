42ee6e0657d441d7b4f9d8efd8fd48c7c5e6e9af
Vince Suriani <chenzorama@hotmail.com>
2021-07-19 11:03:02 -0400

    it was an ESLINT rule

--- pages/image-page.js
+++ pages/image-page.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import styles from '../styles/image-page.module.css'
 
 export default function Home() {
   return (
@@ -27,6 +28,28 @@ export default function Home() {
             <p>CDN image Below</p>
             <Image src="https://cdn.tollbrothers.com/communities/13526/images-resized/004_Toll_5_23_17_Alcott_conversion1_1800.jpg" alt="Toll Logo" width={1222} height={815} fakeattribute="testing" unoptimized={true} /> 
         </div>
+
+        <div>
+          <p>SRC SET</p>
+          <div className={styles.imageholder}>
+          <Image srcSet="https://cdn.tollbrothers.com/communities/13526/images-resized/Reserve-at-Center-Square-Villages-Collection-Barbour-Farmhouse-Kitchen-and-Living-Room-2_conversion1_920.jpg 768w,
+            https://cdn.tollbrothers.com/communities/13526/images-resized/Reserve-at-Center-Square-Villages-Collection-Barbour-Farmhouse-Kitchen-and-Living-Room-2_conversion1_1920.jpg 800w" 
+            sizes="(min-width: 800px) 800px,100vw" 
+            src="https://cdn.tollbrothers.com/communities/13526/images-resized/Reserve-at-Center-Square-Villages-Collection-Barbour-Farmhouse-Kitchen-and-Living-Room-2_conversion1_1920.jpg" 
+            className="community-hero-img" 
+            layout='fill'
+            alt="Creekside at Blue Bell" 
+            fakeattribute="testing" unoptimized={true} />
+            </div>
+        </div>
+
+
+        <div>
+            <p>pure img tag</p>
+            <img src="https://cdn.tollbrothers.com/communities/13526/images-resized/004_Toll_5_23_17_Alcott_conversion1_1800.jpg" /> 
+        </div>
+
+
 
       </main>
 
