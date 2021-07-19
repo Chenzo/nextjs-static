ccb5eeb2f01eceb6de786e1a06df96e44139e530
Vince Suriani <chenzorama@hotmail.com>
2021-07-19 10:47:08 -0400

    working it seems

--- pages/image-page.js
+++ pages/image-page.js
@@ -20,7 +20,12 @@ export default function Home() {
 
         <div>
             <p>Image Below</p>
-            <Image src="/img/vercel.svg" alt="Toll Logo" width={200} height={50} /> 
+            <Image src="/img/vercel.svg" alt="Toll Logo" width={200} height={50} fakeattribute="testing" unoptimized={true} /> 
+        </div>
+
+        <div>
+            <p>CDN image Below</p>
+            <Image src="https://cdn.tollbrothers.com/communities/13526/images-resized/004_Toll_5_23_17_Alcott_conversion1_1800.jpg" alt="Toll Logo" width={1222} height={815} fakeattribute="testing" unoptimized={true} /> 
         </div>
 
       </main>
